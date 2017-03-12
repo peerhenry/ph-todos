012e12851e50016cb20386f587429320cc1f581f
peerhenry <peerhenry@gmail.com>
2017-03-12 22:43:24 +0100

    server stores todos

--- routes/todos.js
+++ routes/todos.js
@@ -1,11 +1,40 @@
 var express = require('express');
 var router = express.Router();
 
+var counter = 1;
+var todos = [];
+
+function addTodo(title, done){
+  todos.push({
+    id: counter++,
+    title: title,
+    done: done
+  });
+}
+
+/* initialize todos */
+addTodo('chillout', true);
+addTodo('brush', false);
+addTodo('mongodb', false);
+addTodo('TotalNote', false);
+
+/* GET todos */
+router.get('/', function(request, response) {
+  response.send({ todos: todos });
+});
+
+/* POST new todo */
 router.post('/addtodo/', function(request, response) {
-  var todo = request.body.todo;
+  var success = false;
+  var respMsg = 'Add todo failed!';
+  var title = request.body.title;
+  if(title){
+    addTodo(title, false);
+    respMsg = 'Todo ' + title + ' succesfully added.';
+    success = true;
+  }
   response.contentType('json');
-  var respMsg = 'You posted: ' + todo;
-  response.send({message: respMsg});
+  response.send({ success: success, message: respMsg });
 });
 
 module.exports = router;
