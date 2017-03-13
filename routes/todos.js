49bb84b0043b9fe8b82b5d55be516ec9342e4230
peerhenry <peerhenry@gmail.com>
2017-03-13 18:07:58 +0100

    todos deletion logic

--- routes/todos.js
+++ routes/todos.js
@@ -5,10 +5,18 @@ var counter = 1;
 var todos = [];
 
 function addTodo(title, done){
-  todos.push({
+  var newTodo = {
     id: counter++,
     title: title,
     done: done
+  };
+  todos.push(newTodo);
+  return newTodo;
+}
+
+function deleteTodo(todoId){
+  todos = todos.filter(function(todo){
+    return todo.id != todoId;
   });
 }
 
@@ -18,7 +26,7 @@ addTodo('brush', false);
 addTodo('mongodb', false);
 addTodo('TotalNote', false);
 
-/* GET todos */
+/* GET all todos */
 router.get('/', function(request, response) {
   response.send({ todos: todos });
 });
@@ -26,15 +34,30 @@ router.get('/', function(request, response) {
 /* POST new todo */
 router.post('/addtodo/', function(request, response) {
   var success = false;
-  var respMsg = 'Add todo failed!';
+  var message = 'Add todo failed!';
   var title = request.body.title;
+  var newTodo;
   if(title){
-    addTodo(title, false);
-    respMsg = 'Todo ' + title + ' succesfully added.';
+    newTodo = addTodo(title, false);
+    message = 'Todo ' + title + ' succesfully added.';
     success = true;
   }
   response.contentType('json');
-  response.send({ success: success, message: respMsg });
+  response.send({ success: success, message: message, todo: newTodo });
+});
+
+/* POST delete todo */
+router.post('/deletetodo/', function(request, response) {
+  var success = false;
+  var message = 'Please pass a valid id!';
+  var todoId = request.body.id;
+  if(todoId){
+    deleteTodo(todoId);
+    message = 'Todo ' + todoId + ' succesfully deleted.';
+    success = true;
+  }
+  response.contentType('json');
+  response.send({ success: success, message: message });
 });
 
 module.exports = router;
